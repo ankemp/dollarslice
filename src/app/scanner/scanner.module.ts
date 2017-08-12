7418d83690882b9ef6c48f8b1e704e308c3c4428
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-12 19:08:16 -0400

    Adding file-storage to scanner.module

--- src/app/scanner/scanner.module.ts
+++ src/app/scanner/scanner.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './scanner-routing.module';
+import { AngularFireModule } from 'angularfire2';
 
 import { CameraService } from '../services/camera.service';
+import { FileStorageService } from '../services/file-storage.service';
 
 import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule,
   ],
   declarations: [
     CameraComponent
   ],
-  providers: [CameraService]
+  providers: [CameraService, FileStorageService]
 })
 export class ScannerModule { }
