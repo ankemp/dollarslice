b47a245a4e34542a21414d3dc504755df3197ad2
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-12 17:30:11 -0400

    Starting initial camera functionality

--- src/app/scanner/camera/camera.component.ts
+++ src/app/scanner/camera/camera.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { CameraService } from '../../services/camera.service';
 
@@ -7,13 +7,51 @@ import { CameraService } from '../../services/camera.service';
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.css']
 })
-export class CameraComponent implements OnInit {
+export class CameraComponent implements OnInit, OnDestroy {
+  private player;
+  private snapshot;
+  private context;
+  imageTaken = false;
 
   constructor(
-    camera: CameraService
+    public camera: CameraService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    // should show a reason to request first, then fire request.
+    this.camera.requestPermission()
+      .then(() => {
+        this.player = document.getElementById('viewport');
+        this.snapshot = document.getElementById('snapshot');
+        this.context = this.snapshot.getContext('2d');
+        this.startStream();
+      });
   }
 
+  ngOnDestroy(): void {
+    this.stopStream();
+  }
+
+  private startStream(): void {
+    this.camera.getStream()
+      .then((stream: MediaStream) => {
+        this.player.srcObject = stream;
+      });
+  }
+
+  private stopStream(): void {
+    this.player.srcObject.getVideoTracks()
+      .forEach(track => track.stop());
+  }
+
+  capture(): void {
+    this.context.drawImage(this.player, 0, 0, this.snapshot.width, this.snapshot.height);
+    this.stopStream();
+    this.imageTaken = true;
+  }
+
+  reset(): void {
+    this.imageTaken = false;
+    this.startStream();
+  }
 }
