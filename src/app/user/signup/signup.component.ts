c1fc3ddd0ae414afffcfee086a3e3ae5e51808c8
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-13 20:23:16 -0400

    Adding dom, wiring, and styling to signup.component - #3

--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignUpComponent implements OnInit {
+  public phoneNumber = '';
+  public signUpSubmitted = false;
+  public confirmationCode = '';
+  public confirmationSubmitted = false;
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.userService.registerRecaptcha();
+  }
+
+  resetSignUp(): void {
+    this.signUpSubmitted = false;
+  }
+
+  signUpSubmit(): void {
+    this.signUpSubmitted = true;
+    if (this.phoneNumber.length === 10) {
+      this.phoneNumber = `+1${this.phoneNumber}`;
+    }
+    this.userService.signIn(this.phoneNumber);
+  }
+
+  signUpConfirm(): void {
+    this.confirmationSubmitted = true;
+    this.userService.signInConfirmation(this.confirmationCode);
   }
 
 }
