ff8e871034f14328a9fbc7dc6a7f845dfb351902
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-15 22:39:34 -0400

    WIP profile - #3

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { UserService } from '../../services/user.service';
+import { FileStorageService } from '../../services/file-storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  public user: Observable<firebase.User>;
+  public activeField = '';
+  public profileFields = { displayName: '', email: '', photoURL: '' };
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private fileService: FileStorageService,
+  ) {
+    this.user = userService.user;
+  }
 
   ngOnInit() {
+    this.user.subscribe(state => {
+      if (state) {
+        this.profileFields.displayName = state.displayName;
+        this.profileFields.email = state.email;
+        this.profileFields.photoURL = state.photoURL;
+      }
+    });
+  }
+
+  endEdit(): void {
+    this.activeField = '';
+  }
+
+  startEdit(fieldName: string): void {
+    this.activeField = fieldName;
+  }
+
+  updateEmail(): void {
+    const email = this.profileFields.email;
+    // do email validation
+    this.userService.updateProfile('email', email);
+  }
+
+  updateDisplayName(): void {
+    const displayName = this.profileFields.displayName;
+    // do displayName validation
+    this.userService.updateProfile('displayName', displayName);
+  }
+
+  updateProfileImage(): void {
+    // this.fileService.upload().then(() => {
+    //   this.userService.updateProfile('profileURL', )
+    // })
   }
 
 }
