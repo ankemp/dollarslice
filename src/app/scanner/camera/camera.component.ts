9883be97ad77562bf2c6bbca9bdf3582a58f150c
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-15 22:38:39 -0400

    Adding styling and dom updates for camera.component - #4 Adding status indicator and separate button for uploading and retaking photo of dollar - #1

--- src/app/scanner/camera/camera.component.ts
+++ src/app/scanner/camera/camera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 import { CameraService } from '../../services/camera.service';
 
@@ -9,8 +10,10 @@ import { CameraService } from '../../services/camera.service';
 })
 export class CameraComponent implements OnInit, OnDestroy {
   imageTaken = false;
+  image: FirebaseObjectObservable<any>;
 
   constructor(
+    private db: AngularFireDatabase,
     public camera: CameraService,
   ) { }
 
@@ -37,8 +40,14 @@ export class CameraComponent implements OnInit, OnDestroy {
 
   capture(): void {
     this.camera.capture();
-    this.camera.save();
     this.imageTaken = true;
+  }
+
+  upload(): void {
+    this.camera.save()
+      .then(key => {
+        this.image = this.db.object(`images/${key}`);
+      });
   }
 
   reset(): void {
