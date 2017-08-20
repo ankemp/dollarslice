3a02e4653b2be52f6959e9112b5903a48525b6a5
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-20 09:39:27 -0400

    Adding login flow

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public phoneNumber = '';
+  public signInSubmitted = false;
+  public captchaSubmitted = false;
+  public confirmationCode = '';
+  public confirmationSubmitted = false;
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.userService.registerRecaptcha();
+  }
+
+  resetSignIn(): void {
+    this.signInSubmitted = false;
+  }
+
+  signInSubmit(): void {
+    this.signInSubmitted = true;
+    if (this.phoneNumber.length === 10) {
+      this.phoneNumber = `+1${this.phoneNumber}`;
+    }
+    this.userService.signIn(this.phoneNumber)
+      .then(() => {
+        this.captchaSubmitted = true;
+      })
+      .catch(err => {
+        // handle error
+      });
+  }
+
+  signInConfirm(): void {
+    this.confirmationSubmitted = true;
+    this.confirmationCode = this.confirmationCode.toString();
+    this.userService.signInConfirmation(this.confirmationCode)
+      .then(() => {
+        // Welcome & redirect to profile? Home?
+      })
+      .catch(err => {
+        // handle error
+      });
   }
 
 }
