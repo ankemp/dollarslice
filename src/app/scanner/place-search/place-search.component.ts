1e72dc573338ceb839584847eeec63a7aacc8265
Andrew Kemp <andrew.kemp@bynd.com>
2017-08-29 15:42:53 -0400

    Adding initial functionality to place-search component

--- src/app/scanner/place-search/place-search.component.ts
+++ src/app/scanner/place-search/place-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
+
+import { LocationService } from '../../services/location.service';
 
 @Component({
   selector: 'app-place-search',
@@ -7,10 +11,33 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PlaceSearchComponent implements OnInit {
   @Input() dollar: FirebaseObjectObservable<any>;
+  public yelpSearch: FirebaseListObservable<any>;
+  public coords = new Subject<Coordinates>();
 
-  constructor() { }
+  constructor(
+    public db: AngularFireDatabase,
+    public location: LocationService
+  ) { }
 
   ngOnInit() {
+    this.coords.subscribe(coords => {
+      console.log('Coords: ', coords);
+    });
+  }
+
+  findMe(highAccuracy = false): void {
+    this.location.getLocation(highAccuracy)
+      .then((coords: Coordinates) => {
+        this.coords.next(coords);
+      })
+      .catch((err: PositionError) => {
+        console.error(err);
+      });
+  }
+
+  searchYelp(): void {
+    this.db.list('/yelp-search')
+      .push({ status: 'query', location: '' });
   }
 
 }
