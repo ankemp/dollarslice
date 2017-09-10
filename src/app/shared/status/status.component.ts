082d1a991f1ad4a0b7aeaf43b9a40ca313e21645
Andrew Kemp <andrew@ankemp.com>
2017-09-10 17:05:07 -0400

    Updates to status.component and how it's used

--- src/app/shared/status/status.component.ts
+++ src/app/shared/status/status.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
@@ -6,9 +6,20 @@ import { FirebaseObjectObservable } from 'angularfire2/database';
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.css']
 })
-export class StatusComponent {
+export class StatusComponent implements OnInit {
   @Input() object: FirebaseObjectObservable<any>;
+  public isComplete = false;
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.object.subscribe(o => {
+      if (o.status === 'complete') {
+        setTimeout(() => {
+          this.isComplete = true;
+        }, 1000);
+      }
+    });
+  }
 
 }
