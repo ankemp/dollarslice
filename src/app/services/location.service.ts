9d52ebe3e711d61ce8ca243da9b510e3d5b3f4a7
Andrew Kemp <andrew@ankemp.com>
2017-10-14 15:52:07 -0400

    Refactoring location.service

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireObject, AngularFireList } from 'angularfire2/database';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as firebase from 'firebase/app';
 
@@ -29,51 +29,32 @@ export class LocationService {
     }
   }
 
-  private getQuick(): Promise<Coordinates | PositionError> {
+  getLocation(enableHighAccuracy = false): Promise<Coordinates | PositionError> {
     return new Promise((Resolve, Reject) => {
       this._navigator.geolocation
-        .getCurrentPosition(position => {
-          return Resolve(position.coords);
+        .getCurrentPosition(({ coords }) => {
+          this.coords.next(coords);
+          Resolve(coords);
         }, err => {
-          return Reject(err);
-        });
+          Reject(err);
+        }, <PositionOptions>{ enableHighAccuracy });
     });
-  }
-
-  private getAccurate(): Promise<Coordinates | PositionError> {
-    const options = <PositionOptions>{
-      enableHighAccuracy: true,
-    };
-    return new Promise((Resolve, Reject) => {
-      this._navigator.geolocation
-        .getCurrentPosition(position => {
-          return Resolve(position.coords);
-        }, err => {
-          return Reject(err);
-        }, options);
-    });
-  }
-
-  getLocation(highAccuracy = false): Promise<Coordinates | PositionError> {
-    const position = highAccuracy ? this.getAccurate() : this.getQuick();
-    position.then((coords: Coordinates) => {
-      this.coords.next(coords);
-    });
-    return position;
   }
 
   private get locationCollection(): AngularFirestoreCollection<any> {
     return this.sdb.collection('location');
   }
 
+  lookup(id: string): AngularFirestoreDocument<any> {
+    this.locationKey.next(id);
+    return this.locationCollection.doc(id);
+  }
+
   create(location: any): Promise<string | Error> {
     return new Promise((Resolve, Reject) => {
       const { distance, distance_unit, is_closed, id, ...data } = location;
-      this.locationCollection.doc(id).set({ ...data })
-        .then((ref) => {
-          this.locationKey.next(id);
-          Resolve(id);
-        })
+      this.lookup(id).set({ ...data })
+        .then(_ => Resolve(id))
         .catch(Reject);
     });
   }
@@ -81,7 +62,7 @@ export class LocationService {
   findOrCreate(location: any): Promise<string | Error> {
     return new Promise((Resolve, Reject) => {
       const { id } = location;
-      const docRef = this.locationCollection.doc(id);
+      const docRef = this.lookup(id);
       docRef.ref.get()
         .then(({ exists }) => exists ? null : this.create(location))
         .then(_ => Resolve(id))
@@ -89,7 +70,7 @@ export class LocationService {
     });
   }
 
-  private checkInList(): AngularFireList<any> {
+  private get checkInList(): AngularFireList<any> {
     return this.db.list('check-in');
   }
 
@@ -108,13 +89,12 @@ export class LocationService {
      * serial -> users
      * serial -> locations
      */
-    return this.checkInList()
-      .push({
-        timestamp: firebase.database.ServerValue.TIMESTAMP,
-        userKey,
-        locationKey,
-        serialKey,
-      });
+    return this.checkInList.push({
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      userKey,
+      locationKey,
+      serialKey,
+    });
   }
 
 }
