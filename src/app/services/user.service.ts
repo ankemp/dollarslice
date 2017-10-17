dfcdb8b04b9a57c9b8fc9bbb3de0b95a43788b8d
Andrew Kemp <andrew@ankemp.com>
2017-10-16 20:44:00 -0400

    Refactoring user.service in prep to move user details to firestore

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as firebase from 'firebase/app';
@@ -9,15 +10,19 @@ export class UserService {
   private confirmation: firebase.auth.ConfirmationResult;
   private recaptchaVerifier: firebase.auth.RecaptchaVerifier;
   public user: Observable<firebase.User>;
+  private profileRef: AngularFirestoreDocument<any>;
+  public profile: Observable<any>;
   public userKey = new BehaviorSubject<string>(null);
 
   constructor(
     private afAuth: AngularFireAuth,
+    private sdb: AngularFirestore
   ) {
     this.user = afAuth.authState;
     this.user.subscribe(state => {
       if (state) {
         this.userKey.next(state.uid);
+        this.lookup(state.uid);
       }
     });
   }
@@ -32,7 +37,7 @@ export class UserService {
         .signInWithPhoneNumber(phoneNumber, this.recaptchaVerifier)
         .then(confirmationResult => {
           this.confirmation = confirmationResult;
-          return Resolve();
+          Resolve();
         })
         .catch(err => Reject(err));
 
@@ -43,13 +48,38 @@ export class UserService {
     return new Promise((Resolve, Reject) => {
       this.confirmation
         .confirm(confirmCode)
-        .then(result => Resolve())
+        .then(_ => Resolve())
         .catch(err => Reject(err));
     });
   }
 
   logout(): void {
     this.afAuth.auth.signOut();
+  }
+
+  private get userCollection(): AngularFirestoreCollection<any> {
+    return this.sdb.collection('user');
+  }
+
+  lookup(uid: string): AngularFirestoreDocument<any> {
+    const currentUid = this.userKey.getValue();
+    if (currentUid !== uid) {
+      this.profileRef = this.userCollection.doc(uid);
+      this.profile = this.profileRef.valueChanges();
+    }
+    return this.profileRef;
+  }
+
+  private updateDB(uid: string, fields: any): Promise<void> {
+    const { displayName, email, emailVerified, phoneNumber, photoURL } = fields;
+    const profile = Object.assign({}, { displayName }, { email }, { emailVerified }, { phoneNumber }, { photoURL });
+    const docRef = this.userCollection.doc(uid);
+    return docRef.ref.get().then(document => {
+      if (document.exists) {
+        return docRef.update({ ...profile, timestamp: firebase.firestore.FieldValue.serverTimestamp() });
+      }
+      return docRef.set({ ...profile, timestamp: firebase.firestore.FieldValue.serverTimestamp() });
+    });
   }
 
   updateProfile(field: string, value: string): Promise<null | Error> {
